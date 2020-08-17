802c9ade83a5eb35fef3c3bc20ee10187a900a27
RaunakDune <RaunakS@hotmail.com>
2020-08-17 12:26:40 -0500

    Starting to add POI agents

--- disease_model.py
+++ disease_model.py
@@ -60,6 +60,46 @@ class DiseaseAgent(Agent):
             include_center=False)
         new_position = self.random.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
+
+class POIAgent(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        if random.randint(0,100) < 10:
+            self.state = 2
+        else:
+            self.state = random.randint(0, 1)
+    
+    def step(self):
+        self.move()
+        if self.state < 4:
+            self.spread_disease()
+    
+    def spread_disease(self):
+        neighbours = self.model.grid.get_cell_list_contents([self.pos])
+
+        if (len(neighbours) > 1):
+            next_agent = self.random.choice(neighbours)
+            if self.state == 3:
+                return
+            if next_agent.state == 3 and self.state != 3:
+                self.state = (self.state + 1) % 4
+                return
+            if (next_agent.state > 1) and (self.state == 0):
+                self.state += 1
+                return
+            if (next_agent.state == 0) and (self.state > 1):
+                next_agent.state += 1
+                return
+            next_agent.state = (next_agent.state + 1) % 4
+            self.state = (self.state + 1) % 4
+    
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,
+            include_center=False)
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
         
 
 class DiseaseModel(Model):
